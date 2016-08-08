6f73f85c0e38c2f7298e29e1fd3b8a972fa6ba13
chenrobert <a298316101@163.com>
2016-08-08 21:46:39 +0800

    hello to world by  shiny 2

---
+++ helloi/server.R
@@ -0,0 +1,26 @@
+rsconnect::setAccountInfo(name='chenrobert', token='CBFB0E0D67C0917EAAEE51470B946EC2', secret='+a6Y0U83ETsqH4XJtsh+wGBR3MlCojuj9iF/nvB+')#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+   
+  output$distPlot <- renderPlot({
+    
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2] 
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+  })
+  
+})
